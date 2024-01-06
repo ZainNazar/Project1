898639f5738ec59b4dd088fa4be8ce59273a4d19
zalkh <zalkh@10.0.0.55>
2024-01-05 19:06:20 -0600

    add method (a,b)

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -8,6 +8,14 @@ public class TestDemo {
 		System.out.println("Hello Batch 11");
 		
 		System.out.println("Hello Team 2");
+		
+		
+	}
+	
+	// stash the method
+	
+	public void add(int a , int b) {
+		System.out.println(a+b);
 	}
 
 	
