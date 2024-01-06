74cb3b252dab7d71c7486b556219527f8aecdd5b
zalkh <zalkh@10.0.0.55>
2024-01-05 18:59:17 -0600

    add name

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -2,6 +2,7 @@ package demo;
 
 public class TestDemo {
 
+	//Zain
 	public static void main(String[] args) {
 
 		System.out.println("Hello Batch 11");
@@ -9,16 +10,6 @@ public class TestDemo {
 		System.out.println("Hello Team 2");
 	}
 
-	// New code
-	public void add(int a, int b) {
-		System.out.println(a + b);
-	}
-
-	// Example Stash
-
-	public void name(String name) {
-		System.out.println(name);
-
-	}
+	
 
 }
